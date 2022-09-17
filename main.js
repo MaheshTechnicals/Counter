31f4e4420eadb9ac5a133ef78a74fde3dfdaee20
msvarma10001@gmail.com <msvarma10001@gmail.com>
2022-09-17 20:33:14 +0530

    Rdy to use

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+let num = document.querySelector("h3")
+let btns = document.querySelectorAll("button")
 
+
+btns.forEach((b) => {
+  b.addEventListener("click", (e) => {
+    let key = e.currentTarget.classList
+
+    let out = Number(num.innerText)
+
+    if (key == "btn1") {
+      out--
+      num.style.color = "red"
+    }
+    else if (key == "btn2") {
+      out = 0
+      num.style.color = "white"
+    }
+    else if (key == "btn3") {
+      out++
+      num.style.color = "green"
+
+    }
+
+    num.innerText = out
+  })
+})
